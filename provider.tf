b97e27d813eee67a7a5abf6c76c4b9e84f5ad025
rotundbadger <147494002+rotundbadger@users.noreply.github.com>
2025-07-29 14:52:08 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,13 @@ terraform {
     key          = "terraform_state"
     region       = "us-east-1"
     use_lockfile = true
+ default_tags {
+   tags = {
+     Environment = "Test"
+     Owner       = "TFProviders"
+     Project     = "Test"
+   }
+
   }
   required_providers {
     aws = {
